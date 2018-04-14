463a5cade272b1e22fcfb8f7d3723c8d86982bc7
Eric Strathmeyer <eric@vawks.com>
2018-04-14 04:46:48 -0700

    Handle Knock messge

--- Update.elm
+++ Update.elm
@@ -1,12 +1,27 @@
 module Update exposing (..)
 
-import Model
+import Model exposing (Model, Player, Round)
 
 
 type Msg
-    = Knock Model.Player
+    = Knock Player
 
 
-update : Msg -> Model.Model -> Model.Model
+addKnocker : Player -> Round -> Round
+addKnocker player round =
+  { round | knocker = Just player }
+
+
+update : Msg -> Model -> Model
 update msg model =
-    model
+  let
+    round = List.head model.rounds
+    tail = Maybe.withDefault [] (List.tail model.rounds)
+  in
+    case round of
+      Nothing ->
+        model
+      Just r ->
+        case msg of
+          Knock player ->
+            { model | rounds = (addKnocker player r) :: tail }
