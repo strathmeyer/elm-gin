8628a4486721a84f02f1ca9cd64b42a949a9ca76
Eric Strathmeyer <eric@vawks.com>
2019-02-06 14:37:56 -0800

    Upgrade to Elm 0.19 (#5)

--- Main.elm
+++ Main.elm
@@ -1,15 +1,14 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Html
-import View
+import Browser
 import Model
 import Update
+import View
 
-
-main : Program Never Model.Model Update.Msg
+main : Program () Model.Model Update.Msg
 main =
-  Html.beginnerProgram
-    { model = Model.init
-    , view = View.view
-    , update = Update.update
-    }
+    Browser.sandbox
+        { init = Model.init
+        , view = View.view
+        , update = Update.update
+        }
--- Model.elm
+++ Model.elm
@@ -1,21 +1,24 @@
-module Model
-  exposing
+module Model exposing
     ( GameState(..)
     , Model
     , init
     , initNextRound
     )
 
+import Model.Player as Player exposing (Player(..))
+import Model.Round as Round exposing (Round)
+import Model.Score as Score exposing (Score)
 
-import Model.Player as Player exposing ( Player(..) )
-import Model.Round as Round exposing ( Round )
-import Model.Score as Score exposing ( Score )
 
+type GameState
+    = InProgress
+    | Completed
 
-type GameState = InProgress | Completed
 
 
 -- TODO: use player names
+
+
 type alias Model =
     { playerOneName : String
     , playerTwoName : String
@@ -29,28 +32,28 @@ type alias Model =
 
 initNextRound : Model -> Model
 initNextRound model =
-  let
-    nextDealer =
-      case List.head model.rounds of
-        Nothing ->
-          PlayerOne
+    let
+        nextDealer =
+            case List.head model.rounds of
+                Nothing ->
+                    PlayerOne
 
-        Just round ->
-          Player.other round.dealer
+                Just round ->
+                    Player.other round.dealer
 
-    nextRound =
-      Round.init nextDealer
-  in
+        nextRound =
+            Round.init nextDealer
+    in
     { model | rounds = nextRound :: model.rounds }
 
 
 init : Model
 init =
-  Model
-    ""
-    ""
-    [ Round.init PlayerOne ]
-    InProgress
-    Score.init
-    Score.init
-    Score.init
+    Model
+        ""
+        ""
+        [ Round.init PlayerOne ]
+        InProgress
+        Score.init
+        Score.init
+        Score.init
